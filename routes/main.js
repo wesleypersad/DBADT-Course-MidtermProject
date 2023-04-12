b5bd584b5d20025f54562b19133b5770dea46c01
Wesley Persad <26929215+wesleypersad@users.noreply.github.com>
2023-04-12 15:06:28 +0100

    Routes rewritten as async functions

--- routes/main.js
+++ routes/main.js
@@ -4,91 +4,83 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("index.ejs")
     });
-    app.get("/datatype", function (req, res) {
+    app.get("/datatype", async function (req, res) {
         //searching in the database
         //let word = ['%' + req.query.keyword + '%'];
         //let sqlquery = "SELECT * FROM foods WHERE fname LIKE ?";
         let sqlquery = "SELECT * FROM DataType";
         // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                console.log('Calling datatype.ejs');
-                res.render('datatype.ejs', { datatypes: result });
+        try {
+            const result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            console.log('Calling datatype.ejs');
+            res.render('datatype.ejs', { datatypes: result });
+        } catch (err) {
+            return console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         //db.end();
     });
-    app.get("/ethnicity", function (req, res) {
-        //searching in the database
-        let sqlquery = "SELECT * FROM Ethnicity";
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('ethnicity.ejs', { ethnicity: result });
+    app.get("/ethnicity", async function (req, res) {
+        try {
+            //searching in the database
+            let sqlquery = "SELECT * FROM Ethnicity";
+            // execute sql query
+            let result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            res.render('ethnicity.ejs', { ethnicity: result });
+        } catch (err) {
+            return console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         //db.end();
     });
-    app.get("/food", function (req, res) {
-        //searching in the database
-        let sqlquery = "SELECT * FROM Food";
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                console.log(req.query.keyword);
-                res.render('food.ejs', { food: result, search:'' });
+    app.get("/food", async function (req, res) {
+        try {
+            //searching in the database
+            let sqlquery = "SELECT * FROM Food";
+            // execute sql query
+            const result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
-        // end the connection
-        //db.end();
+            console.log(req.query.keyword);
+            res.render('food.ejs', { food: result, search:'' });
+        } catch (err) {
+            console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
     });
-    app.get("/nutrient", function (req, res) {
+    app.get("/nutrient", async function (req, res) {
         //searching in the database
         let sqlquery = "SELECT * FROM Nutrient";
         // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('nutrient.ejs', { nutrient: result, search: '' });
+        try {
+            let result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
-        // end the connection
-        //db.end();
+            res.render('nutrient.ejs', { nutrient: result, search: '' });
+        } catch (err) {
+            return console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
     });
-    app.get("/purchases", function (req, res) {
+    app.get("/purchases", async function (req, res) {
         //searching in the database
         //let sqlquery = "SELECT * FROM Purchases LIMIT 20";
         // Reproduce original table from subqueries from 3 normalized tables
@@ -104,23 +96,20 @@ module.exports = function (app) {
         ('Standard_error'*100/'Est_av_qty') AS Res_standard_error \
         FROM Purchases";
         // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('purchases.ejs', { purchases: result, search: '' });
+        try {
+            let result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
-        // end the connection
-        //db.end();
+            res.render('purchases.ejs', { purchases: result, search: '' });
+        } catch (err) {
+            console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
     });
-    app.get("/expenditures", function (req, res) {
+    app.get("/expenditures", async function (req, res) {
         //searching in the database
         //let sqlquery = "SELECT * FROM Expenditures LIMIT 20";
         // Reproduce original table from subqueries from 3 normalized tables
@@ -136,23 +125,20 @@ module.exports = function (app) {
         ('Standard_error'*100/'Est_av_exp') AS Res_standard_error \
         FROM Expenditures";
         // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('expenditures.ejs', { expenditures: result, search: '' });
+        try {
+            const result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
-        // end the connection
-        //db.end();
+            res.render('expenditures.ejs', { expenditures: result, search: '' });
+        } catch (err) {
+            return console.error("Error found"
+            + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
     });
-    app.get("/intakes", function (req, res) {
+    app.get("/intakes", async function (req, res) {
         //searching in the database
         //let sqlquery = "SELECT * FROM Intakes LIMIT 20";
         // Reproduce original table from subqueries from 4 normalized tables
@@ -162,96 +148,92 @@ module.exports = function (app) {
         (SELECT Nutrient FROM Nutrient WHERE Intakes.Nutrient=Nutrient.ID) AS Nutrient, \
         Estimate_of_daily_intake, Standard_error, Residual_Standard_Error \
         FROM Intakes";
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('intakes.ejs', { intakes: result, search: '' });
+        try {
+            // execute sql query
+            let result = await db.query(sqlquery);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            res.render('intakes.ejs', { intakes: result, search: '' });
+        } catch (err) {
+            console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         db.end();
     });
-    app.post("/food", function (req, res) {
+    app.post("/food", async function (req, res) {
         //searching in the database
         let word = ['%' + req.body.keyword + '%'];
         let sqlquery = "SELECT * FROM Food WHERE Food LIKE ?";
         // execute sql query
-        db.query(sqlquery, word, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                console.log(req.query.keyword);
-                res.render('food.ejs', { food: result, search: word });
+        try {
+            let result = await db.query(sqlquery, word);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            console.log(req.query.keyword);
+            res.render('food.ejs', { food: result, search: word });
+        } catch (err) {
+            return console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         //db.end();
     });
-    app.post("/nutrient", function (req, res) {
+    app.post("/nutrient", async function (req, res) {
         //searching in the database
         let word = ['%' + req.body.keyword + '%'];
         let sqlquery = "SELECT * FROM Nutrient WHERE Nutrient LIKE ?";
         // execute sql query
-        db.query(sqlquery, word, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('nutrient.ejs', { nutrient: result, search: word });
+        try {
+            let result = await db.query(sqlquery, word);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            res.render('nutrient.ejs', { nutrient: result, search: word });
+        } catch (err) {
+            console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         //db.end();
     });
-    app.post("/intakes", function (req, res) {
-        //searching in the database
-        //let sqlquery = "SELECT * FROM Intakes LIMIT 20";
-        // Reproduce original table from subqueries from 4 normalized tables
-        let word = ['%' + req.body.keyword + '%'];
-        let sqlquery = "SELECT ID, \
-        (SELECT DataType FROM DataType WHERE Intakes.DataType=DataType.ID) AS DataType, Survey_Year, \
-        (SELECT Ethnicity FROM Ethnicity WHERE Intakes.Ethnicity=Ethnicity.ID) AS Ethnicity, \
-        (SELECT Nutrient FROM Nutrient WHERE Intakes.Nutrient=Nutrient.ID) AS Nutrient, \
-        Estimate_of_daily_intake, Standard_error, Residual_Standard_Error \
-        FROM Intakes \
-        HAVING Nutrient LIKE ?";
-        // execute sql query
-        db.query(sqlquery, word, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('intakes.ejs', { intakes: result, search: word });
+    app.post("/intakes", async function (req, res) {
+        try {
+            //searching in the database
+            //let sqlquery = "SELECT * FROM Intakes LIMIT 20";
+            // Reproduce original table from subqueries from 4 normalized tables
+            let word = ['%' + req.body.keyword + '%'];
+            let sqlquery = "SELECT ID, \
+            (SELECT DataType FROM DataType WHERE Intakes.DataType=DataType.ID) AS DataType, Survey_Year, \
+            (SELECT Ethnicity FROM Ethnicity WHERE Intakes.Ethnicity=Ethnicity.ID) AS Ethnicity, \
+            (SELECT Nutrient FROM Nutrient WHERE Intakes.Nutrient=Nutrient.ID) AS Nutrient, \
+            Estimate_of_daily_intake, Standard_error, Residual_Standard_Error \
+            FROM Intakes \
+            HAVING Nutrient LIKE ?";
+            // execute sql query
+            let result = await db.query(sqlquery, word);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            res.render('intakes.ejs', { intakes: result, search: word });
+        } catch (err) {
+            return console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         //db.end();
     });
-    app.post("/purchases", function (req, res) {
+    app.post("/purchases", async function (req, res) {
         //searching in the database
         //let sqlquery = "SELECT * FROM Purchases LIMIT 20";
         // Reproduce original table from subqueries from 3 normalized tables
@@ -268,24 +250,23 @@ module.exports = function (app) {
         ('Standard_error'*100/'Est_av_qty') AS Res_standard_error \
         FROM Purchases \
         HAVING Food LIKE ?";
-        // execute sql query
-        db.query(sqlquery, word, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('purchases.ejs', { purchases: result, search: word });
+        try {
+            // execute sql query
+            const result = await db.query(sqlquery, word);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
+            res.render('purchases.ejs', { purchases: result, search: word });
+        } catch (err) {
+            return console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
         // end the connection
         //db.end();
     });
-    app.post("/expenditures", function (req, res) {
+    app.post("/expenditures", async function (req, res) {
         //searching in the database
         //let sqlquery = "SELECT * FROM Expenditures LIMIT 20";
         // Reproduce original table from subqueries from 3 normalized tables
@@ -302,21 +283,18 @@ module.exports = function (app) {
         ('Standard_error'*100/'Est_av_exp') AS Res_standard_error \
         FROM Expenditures \
         HAVING Food LIKE ?";
-        // execute sql query
-        db.query(sqlquery, word, (err, result) => {
-            if (err) {
-                return console.error("Error found"
-                    + req.query.keyword + "error: " + err.message);
-                res.redirect("/");
-            } else {
-                // if no results found output a message
-                if (result.length == 0) {
-                    console.log("No records found");
-                }
-                res.render('expenditures.ejs', { expenditures: result, search: word });
+        try {
+            // execute sql query
+            const result = await db.query(sqlquery, word);
+            // if no results found output a message
+            if (result.length == 0) {
+                console.log("No records found");
             }
-        });
-        // end the connection
-        //db.end();
+            res.render('expenditures.ejs', { expenditures: result, search: word });
+        } catch (err) {
+            console.error("Error found"
+                + req.query.keyword + "error: " + err.message);
+            res.redirect("/");
+        }
     });
 }
